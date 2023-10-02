195d02a2bcd28b5ec8928db60037308dacb8934b
Neil Cummings <31762828+TryCatchLearn@users.noreply.github.com>
2023-10-02 14:31:23 +0700

    End of section 2

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
---
+++ API/Entities/Product.cs
@@ -0,0 +1,13 @@
+namespace API.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public long Price { get; set; }
+    public string PictureUrl { get; set; }
+    public string Type { get; set; }
+    public string Brand { get; set; }
+    public int QuantityInStock { get; set; }
+}
