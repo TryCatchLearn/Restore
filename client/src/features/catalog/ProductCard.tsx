a5b30ac8fcacbd20887a8f936d624f38d6587db8
Neil Cummings <31762828+TryCatchLearn@users.noreply.github.com>
2023-10-07 10:53:42 +0700

    End of section 12

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -42,7 +42,7 @@ export default function ProductCard({ product }: Props) {
             </CardContent>
             <CardActions>
                 <LoadingButton
-                    loading={status.includes('pendingAddItem' + product.id)} 
+                    loading={status === 'pendingAddItem' + product.id} 
                     onClick={() => dispatch(addBasketItemAsync({productId: product.id}))} 
                     size="small">Add to Cart</LoadingButton>
                 <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
